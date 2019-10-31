58f5741f9a28c33db80a998b34966bee1de98862
noook <me@neilrichter.com>
2019-10-31 14:24:58 +0100

    Initial Commit

---
+++ Share It Fast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Share It Fast
+//
+//  Created by Neil Richter on 31/10/2019.
+//  Copyright © 2019 Neil Richter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Share It FastTests/Share_It_FastTests.swift
@@ -0,0 +1,34 @@
+//
+//  Share_It_FastTests.swift
+//  Share It FastTests
+//
+//  Created by Neil Richter on 31/10/2019.
+//  Copyright © 2019 Neil Richter. All rights reserved.
+//
+
+import XCTest
+@testable import Share_It_Fast
+
+class Share_It_FastTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
