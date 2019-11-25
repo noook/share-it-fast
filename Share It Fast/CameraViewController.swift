28926a21849e0fdb659b4f4d50a3b3e08ef6a5db
noook <me@neilrichter.com>
2019-11-25 12:08:46 +0100

    ✨ Added CameraView Gallery Pick

--- Share It Fast/CameraViewController.swift
+++ Share It Fast/CameraViewController.swift
@@ -9,6 +9,39 @@
 import UIKit
 
 class CameraViewController: UIViewController {
+    var imageView: UIImageView = UIImageView()
+    var pickButton: UIButton = UIButton()
+    var sendButton: UIButton = UIButton()
+    var imagePicker: ImagePicker!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.imageView.contentMode = .scaleAspectFit
+        
+        self.pickButton.setTitle("Pick image", for: .normal)
+        self.pickButton.setTitleColor(.systemBlue, for: .normal)
+        self.pickButton.addTarget(self, action: #selector(showImagePicker(_:)), for: .touchUpInside)
+        
+        self.sendButton.setTitle("Send", for: .normal)
+        self.sendButton.setTitleColor(.systemBlue, for: .normal)
+        self.sendButton.isHidden = true
+
+        self.view.grid(child: self.imageView, x: 0, y: 0, height: 8, width: 12)
+        self.view.grid(child: self.pickButton, x: 4, y: 8, height: 1/2, width: 4)
+        self.view.grid(child: self.sendButton, x: 4, y: 17/2, height: 1/2, width: 4)
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+    }
+    
+    @objc func showImagePicker(_ sender: UIButton) {
+        self.imagePicker.present(from: sender)
+    }
 }
 
+extension CameraViewController: ImagePickerDelegate {
+
+    func didSelect(image: UIImage?) {
+        self.imageView.image = image
+        self.sendButton.isHidden = false
+    }
+}
